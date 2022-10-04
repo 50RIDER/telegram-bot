b3763796ae3f84cbbcc99ad293a9be03349b6a83
i.am.goutham <97418751+killadichandu@users.noreply.github.com>
2022-10-04 08:28:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,18 +4,17 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/@im_goutham_josh</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/im_goutham_josh>GӨЦƬΉΛM ƧΣЯ😎✔</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Kuttu Bot is a open source project. 
-- Source - https://github.com/killadichandu/telegram-bot  
+    SOURCE_TXT = """Source Code - <a href=https://github.com/killadichandu/telegram-bot>Click Here</a>"""
+- Kuttu Bot is a open source project.   
 
 <b>DEVS:</b>
-- <a href=https://t.me/@im_goutham_josh</a>"""
+- <a href=https://t.me/im_goutham_josh>GӨЦƬΉΛM ƧΣЯ✔✔✔</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Kuttu BOT will respond whenever a keyword is found the message
@@ -40,7 +39,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/@im_goutham_josh)</code>
+<code>[Button Text](buttonurl:https://t.me/im_goutham_josh)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
