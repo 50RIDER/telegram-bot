3ead9295c33e73ea91e38e0fe5ec3fb74168764d
i.am.goutham <97418751+GouthamSER@users.noreply.github.com>
2022-10-14 21:12:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/killadichandu/telegram-bot.git /telegram-bot
+  git clone https://github.com/GouthamSER/telegram-bot.git /telegram-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /telegram-bot
